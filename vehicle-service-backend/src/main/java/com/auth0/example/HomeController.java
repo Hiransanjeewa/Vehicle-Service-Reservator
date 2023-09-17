7e0a6c2e757e7f3bea90780e4237d07d952784ff
Hiransanjeewa <hiransanjeewaa@gmail.com>
2023-09-17 19:53:02 +0530

    Asgardio identity provider added because of a connectivity issue in auth0

--- vehicle-service-backend/src/main/java/com/auth0/example/HomeController.java
+++ vehicle-service-backend/src/main/java/com/auth0/example/HomeController.java
@@ -1,6 +1,8 @@
 package com.auth0.example;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,10 +15,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     @GetMapping("/")
-    public String home(Model model, @AuthenticationPrincipal OidcUser principal) {
-        if (principal != null) {
-            model.addAttribute("profile", principal.getClaims());
+    public String currentUserName(Model model, Authentication authentication) {
+        if (authentication != null){
+            DefaultOidcUser userDetails = (DefaultOidcUser) authentication.getPrincipal();
+            model.addAttribute("userName", userDetails.getName());
+            model.addAttribute("IDTokenClaims", userDetails);
         }
-        return "index";
+
+        return "home";
     }
+
 }
