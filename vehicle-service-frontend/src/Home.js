3882ba1325506c834db12839a204cfb9212770ff
Hiransanjeewa <hiransanjeewaa@gmail.com>
2023-09-18 21:34:37 +0530

    Header created without css

---
+++ vehicle-service-frontend/src/Home.js
@@ -0,0 +1,20 @@
+
+import React from 'react';
+import Header from './components/header';
+
+
+
+export default function Home() {
+
+    return(
+        <div>
+          
+                   <Header></Header>
+                  
+
+        
+         
+        </div>
+    )
+
+}
