b75f94abe18856726fe8be1bf0b4382e65c4ad33
Hiransanjeewa <hiransanjeewaa@gmail.com>
2023-09-19 11:59:48 +0530

    Loop created in the all reservations viewing functionality

--- vehicle-service-frontend/src/components/reservations.js
+++ vehicle-service-frontend/src/components/reservations.js
@@ -3,45 +3,78 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import "./header.css"
-
+import  { useState } from 'react';
 
 export default function Reservation() {
+
+    const initialData = [
+        {
+          "book_id": 302,
+          "name": "DjGihantha",
+          "email": "djgihantha@gmail.com",
+          "phone": "4534535324",
+          "date": "2023-09-18T00:00:00.000+00:00",
+          "time": "17:51:12",
+          "location": "Kurunagla dote",
+          "vehicle_no": "DJ-0005",
+          "mileage": 2344,
+          "message": "Gammak thamai"
+        },
+        {
+            "book_id": 303,
+            "name": "GihanthaBro",
+            "email": "djgihantha@gmail.com",
+            "phone": "4534535324",
+            "date": "2023-09-18T00:00:00.000+00:00",
+            "time": "17:51:12",
+            "location": "Kurunagla dote",
+            "vehicle_no": "DJ-0005",
+            "mileage": 2346,
+            "message": "Masa ganakin service karala na"
+          }
+      ];
+    
+      const [data, setData] = useState(initialData);
+    
   return (
-
-
-<table class="table table-dark">
+    <div>
+        <table class="table table-dark">
   <thead>
     <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
+      <th scope="col">Booking_No</th>
+      <th scope="col">Vehicle_No</th>
+      <th scope="col">Date</th>
+      <th scope="col">Time</th>
+      <th scope="col">Location</th>
+      <th scope="col">Mileage</th>
+      <th scope="col">Message</th>
     </tr>
   </thead>
   <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
+         {data.map((item, index) => (
+       
+    <tr key={index}>
+      
+      <td> {item.book_id}</td>
+      <td> {item.vehicle_no}</td>
+      <td> {item.date}</td>
+      <td> {item.time}</td>
+      <td> {item.location}</td>
+      <td> {item.mileage}</td>
+      <td> {item.message}</td>
     </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>Larry</td>
-      <td>the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
+ 
+
+      ))}
+      </tbody>
 </table>
 
 
 
+ 
 
+
+
+</div>
 );
 };
