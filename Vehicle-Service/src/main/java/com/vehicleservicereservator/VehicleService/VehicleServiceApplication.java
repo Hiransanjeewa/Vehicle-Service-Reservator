f66fc01fc9142f947d705fe031b5695e4a679384
Hiransanjeewa <hiransanjeewaa@gmail.com>
2023-09-23 08:59:49 +0530

    Testing the backent with frontend token

--- Vehicle-Service/src/main/java/com/vehicleservicereservator/VehicleService/VehicleServiceApplication.java
+++ Vehicle-Service/src/main/java/com/vehicleservicereservator/VehicleService/VehicleServiceApplication.java
@@ -3,6 +3,10 @@ package com.vehicleservicereservator.VehicleService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.vehicleservicereservator")
@@ -11,5 +15,7 @@ public class VehicleServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(VehicleServiceApplication.class, args);
 	}
+
+
 
 }
