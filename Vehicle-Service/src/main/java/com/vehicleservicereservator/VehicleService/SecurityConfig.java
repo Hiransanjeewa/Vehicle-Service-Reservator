fda52a7399c24747ccabfff0cd12391f5aad9502
Hiransanjeewa <hiransanjeewaa@gmail.com>
2023-11-15 09:13:56 +0530

    Security dependencies have commented temporary

--- Vehicle-Service/src/main/java/com/vehicleservicereservator/VehicleService/SecurityConfig.java
+++ Vehicle-Service/src/main/java/com/vehicleservicereservator/VehicleService/SecurityConfig.java
@@ -1,27 +1,27 @@
 package com.vehicleservicereservator.VehicleService;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.oauth2.jwt.JwtDecoders;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.logout.LogoutHandler;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.web.bind.annotation.CrossOrigin;
-
-import java.io.IOException;
-
-import static org.springframework.security.config.Customizer.withDefaults;
-
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+//import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.oauth2.jwt.JwtDecoders;
+//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+//import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.security.web.authentication.logout.LogoutHandler;
+//import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+//import org.springframework.web.bind.annotation.CrossOrigin;
+//
+//import java.io.IOException;
+//
+//import static org.springframework.security.config.Customizer.withDefaults;
+//
 
 //@Configuration
 //@EnableWebSecurity
